0d860cf25c7eda2c276e8b4ccc28f8e25f68e0ec
Vlad <vladisla1988@gmail.com>
2020-04-22 23:25:11 +0300

    added folder with img and work with fadeto slide up slidedown

--- main.js
+++ main.js
@@ -17,14 +17,18 @@ $(function() {
     //     var e = '.' + element;
     //     e.fadeOut(time);
     // }
-    function elementOut(element, time) {
-        if (time > 5000 || time < 1000 || isNaN(time)) {
-            return false;
-        } else {
-            var className = "." + element;
-            $(className).fadeOut(time);
-        }
-    }
+    // function elementOut(element, time) {
+    //     if (time > 5000 || time < 1000 || isNaN(time)) {
+    //         return false;
+    //     } else {
+    //         var className = "." + element;
+    //         $(className).fadeOut(time);
+    //     }
+    // }
 
-    elementOut('newp', 5000);
+    // elementOut('newp', 5000);
+    $('.img').on('click', function() {
+        $(this).fadeTo(3000, 0.3);
+    });
+    $('.tokio').slideUp(5000).slideDown(5000);
 });
